5b8b838f6e2342d9ec92f2e1e6958035f1a72629
stephen776 <stephenjacobs86@gmail.com>
2023-02-13 10:36:03 -0500

    another comma :(

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -26,7 +26,7 @@ U_NP,              U_NP,              &kp ESC,           &kp RET,           &kp 
 // BUTTON
 #define MIRYOKU_LAYER_BUTTON \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          U_NU,              U_NU,              &kp LGUI,          &kp LALT,          &kp LCTRL          &kp LSHFT,         \
+&kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          U_NU,              U_NU,              &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
