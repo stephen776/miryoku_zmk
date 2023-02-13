d47ec9480cc84041e3a6eeb638b9f9198c3131f7
stephen776 <stephenjacobs86@gmail.com>
2023-02-13 12:28:05 -0500

    remove nice view config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -80,8 +80,3 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp TAB,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-
-// make nice view work 
-&nice_view_spi {
-    cs-gpios = <&pro_micro 0 GPIO_ACTIVE_HIGH>;
-};
