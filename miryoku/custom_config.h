0d54c6da7e1927a8e9c2d468117f7a7f1d8fc5bb
stephen776 <stephenjacobs86@gmail.com>
2023-02-14 05:45:22 -0500

    update thumbs

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -20,7 +20,7 @@
 &kp Q,             &kp W,             &kp E,               &kp R,             &kp T,               &kp Y,              &kp U,               &kp I,             &kp O,             &kp P,                \
 U_MT(LSHFT, A),    U_MT(LCTRL, S),    U_MT(LALT, D),       U_MT(LGUI, F),     &kp G,               &kp H,              U_MT(LGUI, J),       U_MT(LALT, K),     U_MT(LCTRL, L),    U_MT(LSHFT, SQT),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,               &kp V,             &kp B,               &kp N,              &kp M,               &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),  U_LT(U_NAV, RET),  U_LT(U_MOUSE, TAB),  U_LT(U_SYM, BSPC),  U_LT(U_NUM, SPACE),  U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),  U_LT(U_NAV, BSPC), U_LT(U_MOUSE, DEL),  U_LT(U_SYM, RET),   U_LT(U_NUM, SPACE),  U_LT(U_FUN, TAB),  U_NP,              U_NP
 
 
 // TAP
@@ -28,7 +28,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),  U_LT(U_NAV, RET),  U_
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp ESC,           &kp RET,           &kp TAB,           &kp BSPC,          &kp SPACE,         &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              &kp ESC,           &kp BSPC,          &kp DEL,           &kp RET,           &kp SPACE,         &kp TAB,           U_NP,              U_NP
 
 
 // BUTTON
@@ -44,7 +44,7 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp BSPC,          &kp SPACE,         &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp SPACE,         &kp TAB,           U_NP,              U_NP
 
 
 // MOUSE
